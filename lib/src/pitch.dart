7ce21a10ad4d26a7da35f33c245a6af0ae3d9368
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-05-11 16:31:28 -0500

    Implement xml writing on Pitch

--- lib/src/pitch.dart
+++ lib/src/pitch.dart
@@ -1,9 +1,10 @@
+import 'package:music_xml/src/to_music_xml.dart';
 import 'package:xml/xml.dart';
 
 import 'basic_attributes.dart';
 
 /// Representation of a MusicXml Pitch
-class Pitch {
+class Pitch implements ToMusicXml {
   const Pitch({
     required this.step,
     this.alter = 0.0,
@@ -22,4 +23,13 @@ class Pitch {
   final Step step;
   final double alter;
   final int octave;
+
+  @override
+  XmlNode node() {
+    return XmlElement(XmlName('pitch'), [], [
+      XmlElement(XmlName('step'), [], [XmlText(writeStep(step))]),
+      if (alter != 0.0) XmlElement(XmlName('alter'), [], [XmlText('$alter')]),
+      XmlElement(XmlName('octave'), [], [XmlText('$octave')]),
+    ]);
+  }
 }
